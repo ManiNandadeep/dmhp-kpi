5e378cbf8220dd165f7235bda7762860b6093e8b
ManiNandadeep <mani.nandadeep@iiitb.org>
2021-06-18 23:02:43 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialDesignModule } from "./material-design/material-design.module";
+import { NavbarTopComponent } from './components/navbar-top/navbar-top.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, NavbarTopComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
