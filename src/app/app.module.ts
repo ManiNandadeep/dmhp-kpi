2851302d6e79b034b07d229fe51769b82aaa18f9
Prasanna <prasannavenkateshramkumar@gmail.com>
2021-06-19 22:01:09 +0530

    added primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialDesignModule } from "./material-design/material-design.module";
-import { NavbarTopComponent } from './components/navbar-top/navbar-top.component';
-import { CardGroupComponent } from './components/card-group/card-group.component';
+
+import { NavbarTopComponent } from "./components/navbar-top/navbar-top.component";
+import { CardGroupComponent } from "./components/card-group/card-group.component";
 
 @NgModule({
     declarations: [AppComponent, NavbarTopComponent, CardGroupComponent],
