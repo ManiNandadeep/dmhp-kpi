e3c817acb5edb2c96cbb1e6c206a964b30e0cd1c
Prasanna <prasannavenkateshramkumar@gmail.com>
2021-06-30 22:34:39 +0530

    removed primeng styling and finished tbl_distrcitexpense card

--- src/app/components/card-group/card-group.component.ts
+++ src/app/components/card-group/card-group.component.ts
@@ -15,6 +15,7 @@ export class CardGroupComponent implements OnInit {
 
     // budget and expense variables
     totalBudget!: number;
+    totalExpense!: number;
 
     constructor(
         public backendConnectorService: BackendConnectorService,
@@ -43,6 +44,39 @@ export class CardGroupComponent implements OnInit {
                     );
                 }
                 console.log(`hello ${this.totalBudget}`);
+            });
+
+        this.backendConnectorService
+            .getDistrictExpense()
+            .subscribe((districtExpense: any) => {
+                this.totalExpense = 0;
+                for (let i = 0; i < districtExpense.length; ++i) {
+                    this.totalExpense +=
+                        districtExpense[i].ambulatoryService +
+                        districtExpense[i].b3012_StaffNurse +
+                        districtExpense[i].b30114_SocialWorker +
+                        districtExpense[i].b2030_AnnualIncrement +
+                        districtExpense[i].b30137_MedialRedAsst +
+                        districtExpense[i].b3032_Psychiatrists +
+                        districtExpense[i].b30112_Psyst_Counsellor +
+                        districtExpense[i].b3032_PsychiatristsTA +
+                        districtExpense[i].targetIntervention +
+                        districtExpense[i].b30114_SocialWorkerTA +
+                        districtExpense[i].operationExpense +
+                        districtExpense[i].j18_InnovationMH +
+                        districtExpense[i].b30137_WardAsst +
+                        districtExpense[i].infrastucture +
+                        districtExpense[i].b3012_PsyNurse +
+                        districtExpense[i].iec +
+                        districtExpense[i].b10162_Awarness +
+                        districtExpense[i].drugs +
+                        districtExpense[i].miscellanious +
+                        districtExpense[i].equipments +
+                        districtExpense[i].j17_Contingency +
+                        districtExpense[i].training +
+                        districtExpense[i].stateStaff1 +
+                        districtExpense[i].stateStaff2;
+                }
             });
     }
 }
