603ea3eb0a2c7da993d729c44bf64fc880fc7bca
ManiNandadeep <mani.nandadeep@iiitb.org>
2021-07-13 18:38:38 +0530

    Update getDistrictExpense.sql

--- backend/stored-procedures/getDistrictExpense.sql
+++ backend/stored-procedures/getDistrictExpense.sql
@@ -1,8 +1,8 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getDistrictExpense`(
-	  IN display varchar(200),
-    IN group_by varchar(200),
+	IN display varchar(200),
+	IN group_by varchar(200),
     IN agg varchar(200),
-	  IN district_list varchar(200),
+	IN district_list varchar(200),
     IN start_date date,
     IN end_date date,
     IN timeperiod_type varchar(200),
@@ -21,7 +21,33 @@ BEGIN
 		year_type 
     */
     
-    set @statement = CONCAT("select * from DMHPv1.tbl_DistrictExpense where StateId = 17");
+	DECLARE district_id_string varchar(300);
+	DECLARE date_filter_string varchar(300);
+
+	
+	SELECT MIN(ReportingMonthYear) INTO @MinReportingMonthYear FROM DMHPv1.tbl_districtexpense;
+    SELECT MAX(ReportingMonthYear) INTO @MaxReportingMonthYear FROM DMHPv1.tbl_districtexpense;
+    
+	/* Create a district id filter statement string */
+    if(district_list = '') then
+		set @district_id_string = CONCAT("DistrictId IN (select distinct DistrictId from DMHPv1.Districts)");
+	else
+		set @district_id_string = CONCAT("DistrictId IN (",district_list,")");
+	end if;
+    
+    
+     /* Filter by Start date and End Date */
+    if(start_date IS NULL and end_date IS NULL) then 
+		set @date_filter_string = CONCAT("ReportingMonthYear BETWEEN '", @MinReportingMonthYear , "' and '", @MaxReportingMonthYear, "'");
+	elseif(start_date IS NULL) then
+		set @date_filter_string = CONCAT("ReportingMonthYear <= '", end_date, "'");
+	elseif(end_date IS NULL) then
+		set @date_filter_string = CONCAT("ReportingMonthYear >= '", start_date, "'");
+	else 
+		set @date_filter_string = CONCAT("ReportingMonthYear BETWEEN '", start_date, "' and '", end_date , "'");
+	end if;
+    
+    set @statement = CONCAT("select * from DMHPv1.tbl_districtexpense where StateId = 17 and ", @district_id_string, " and ", @date_filter_string);
     
     # select concat(@statement);
     prepare stmt from @statement;
