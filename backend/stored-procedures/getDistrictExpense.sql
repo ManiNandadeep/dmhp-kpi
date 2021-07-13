c293ce16de337123218993d18a73bea74e78ef13
Prasanna Venkatesh <prasannavenkateshramkumar@gmail.com>
2021-07-13 18:48:19 +0530

    Started Aggregation queries on DistrictExpense

--- backend/stored-procedures/getDistrictExpense.sql
+++ backend/stored-procedures/getDistrictExpense.sql
@@ -22,7 +22,10 @@ BEGIN
     */
     
 	DECLARE district_id_string varchar(300);
+    DECLARE agg_string varchar(500);
 	DECLARE date_filter_string varchar(300);
+    
+     SET @agg_string = NULL;
 
 	
 	SELECT MIN(ReportingMonthYear) INTO @MinReportingMonthYear FROM DMHPv1.tbl_districtexpense;
@@ -47,7 +50,34 @@ BEGIN
 		set @date_filter_string = CONCAT("ReportingMonthYear BETWEEN '", start_date, "' and '", end_date , "'");
 	end if;
     
-    set @statement = CONCAT("select * from DMHPv1.tbl_districtexpense where StateId = 17 and ", @district_id_string, " and ", @date_filter_string);
+    
+    
+    
+    /* Aggregation statements */
+    If(FIND_IN_SET('Psychiatrists',agg))THEN
+		SET @agg_string = CONCAT("SUM(B3032_Psychiatrists) as Psychiatrists");
+	END IF;
+    
+    
+     IF(FIND_IN_SET('Psyst_Counsellor',agg))THEN
+		IF(@agg_string is NULL) THEN
+			SET @agg_string = CONCAT("SUM(B30112_Psyst_Counsellor) as Psyst_Counsellor");
+		ELSE
+			SET @agg_string = CONCAT(@agg_string,",SUM(B30112_Psyst_Counsellor) as Psyst_Counsellor");
+		END IF;
+	END IF;
+    
+    IF(FIND_IN_SET('SocialWorker',agg))THEN
+		IF(@agg_string is NULL) THEN
+			SET @agg_string =CONCAT("SUM(B30114_SocialWorker) as SocialWorker");
+		ELSE
+			SET @agg_string = CONCAT(@agg_string,",SUM(B30114_SocialWorker) as SocialWorker");
+		END IF;
+	END IF;
+
+
+    set @statement = CONCAT("select BudgetExpenseId,DistrictId,ReportingMonthYear,", @agg_string ," from DMHPv1.tbl_districtexpense where StateId = 17 and ", @district_id_string, " and ", @date_filter_string, "GROUP BY BudgetExpenseId");
+    
     
     # select concat(@statement);
     prepare stmt from @statement;
