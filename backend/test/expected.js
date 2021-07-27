bf12c31b8abb0f94ec2ec41be26cd516c1bff0f5
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2021-07-27 23:32:25 +0530

    [BACKEND][FEATURE] Json Load in Unit Testing - v1

--- backend/test/expected.js
+++ backend/test/expected.js
@@ -1,27 +1,17 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "../../.env" });
 
+let tbl_training_body_json = require("../sample-json-calls/call_training_stored_procedure.json")
+let tbl_DistrictExpense_json = require("../sample-json-calls/call_districtexpense_storedprocedure.json")
+
 module.exports = {
-    bearerToken:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTYyNzQwMDgxNywiZXhwIjoxNjI3NDA4MDE3fQ.7OE6tLOPuuimHWJdTtnFyGQGvN3v0PGcntHwqx3JhX8",
+    bearerToken :  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTYyNzQwNzYxMSwiZXhwIjoxNjI3NDE0ODExfQ.7O5CUMxaPEXtCDWKuxtjkij_xCwTiykWZe-1RSthgw4",
     authBody: {
-        username: "dmhp",
-        password: process.env.AUTH_PASSWORD,
+        "username":"dmhp",
+        "password":process.env.AUTH_PASSWORD
     },
-    tbl_training_body: {
-        display: "DistrictId,ReportingMonthYear",
-        district_list: "2",
-        event_list: "",
-        target_group_list: "",
-        resource_list: "",
-        start_date: "2020-12-01",
-        end_date: "2020-12-02",
-        timeperiod_type: "quarterly",
-        year_type: "f",
-        group_by: "DistrictId,financial_year,Quarter",
-        facility_list: "",
-    },
-    tbl_training_response: [
+    tbl_training_body : tbl_training_body_json,
+    tbl_training_response : [
         [
             {
                 DistrictId: 2,
@@ -42,17 +32,8 @@ module.exports = {
             changedRows: 0,
         },
     ],
-    tbl_districtExpense_body: {
-        display: "DistrictId",
-        group_by: "DistrictId",
-        agg: "TotalExpense",
-        district_list: "1,2,3",
-        start_date: "2018-01-01",
-        end_date: "2020-01-01",
-        timeperiod_type: "annually",
-        year_type: "f",
-    },
-    tbl_districtExpense_response: [
+    tbl_districtExpense_body : tbl_training_body_json,
+    tbl_districtExpense_response : [
         [
             {
                 DistrictId: 1,
@@ -77,76 +58,5 @@ module.exports = {
             protocol41: true,
             changedRows: 0,
         },
-    ],
-
-    tbl_districtMNS_body: {
-        display: "DistrictId",
-        group_by: "DistrictId",
-        agg: "Head_15",
-        district_list: "27,29",
-        status_list: "",
-        start_date: "2020-01-01",
-        end_date: "2020-01-02",
-        timeperiod_type: "quarterly",
-        year_type: "c",
-    },
-
-    tbl_districtMNS_response: [
-        [
-            {
-                DistrictId: 27,
-                Head_15: 0,
-            },
-            {
-                DistrictId: 29,
-                Head_15: 0,
-            },
-        ],
-        {
-            fieldCount: 0,
-            affectedRows: 0,
-            insertId: 0,
-            serverStatus: 34,
-            warningCount: 0,
-            message: "",
-            protocol41: true,
-            changedRows: 0,
-        },
-    ],
-
-    tbl_MNSAllocation_body: {
-        display: "DistrictId",
-        group_by: "DistrictId",
-        agg: "RHead_15",
-        district_list: "20,27,28",
-        quaterly_list: "1,2",
-        financial_year: "",
-    },
-
-    tbl_MNSAllocation_response: [
-        [
-            {
-                DistrictId: 27,
-                RHead_15: 8.16,
-            },
-            {
-                DistrictId: 28,
-                RHead_15: 3.1100000000000003,
-            },
-            {
-                DistrictId: 20,
-                RHead_15: 6.42,
-            },
-        ],
-        {
-            fieldCount: 0,
-            affectedRows: 0,
-            insertId: 0,
-            serverStatus: 34,
-            warningCount: 0,
-            message: "",
-            protocol41: true,
-            changedRows: 0,
-        },
-    ],
+    ]
 };
